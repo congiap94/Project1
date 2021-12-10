e0738a3e6b0311a80689e2edf62cee5530feaf7c
giap <giap@gmail.com>
2021-12-10 08:45:25 +0700

    meet 27- fix comment 3

--- spec/requests/api/v1/meeting_rooms_spec.rb
+++ spec/requests/api/v1/meeting_rooms_spec.rb
@@ -5,20 +5,24 @@ require 'rails_helper'
 RSpec.describe 'Api::V1::MeetingRooms', type: :request do
   describe 'POST /create' do
     let(:office_1) { create(:office) }
-    let(:user) { create(:user, office: office_1) }
     let(:office_2) { create(:office) }
-    let(:admin) { create(:user, office: office_2) }
-    let!(:office_manager) { create(:office_manager, office: office_2, user: admin) }
-    let!(:meeting_room) { create(:meeting_room, office: office_2, user: admin) }
+    let(:user_1) { create(:user, office: office_1) }
+    let(:admin_1) { create(:user, office: office_1) }
+    let(:admin_2) { create(:user, office: office_2) }
+    let!(:office_manager_1) { create(:office_manager, office: office_1, user: admin_1) }
+    let!(:office_manager_2) { create(:office_manager, office: office_2, user: admin_2) }
+    let!(:meeting_room) { create(:meeting_room, office: office_1, user: admin_1) }
     let(:valid_attribues) do
-      { name: 'Room 1', seat: '9', office_id: office_2.id, user_id: admin.id }
+      { name: 'Room 1', seat: '9', office_id: office_1.id, user_id: admin_1.id }
+    end
+    let(:invalid_attribues) do
+      { name: 'Room 1', seat: '9', office_id: office_1.id, user_id: user_1.id }
     end
 
     context 'Authentication' do
       context 'When OfficeManager not found' do
         it 'raise Could not find OfficeManager' do
-          valid_attribues[:user_id] = user.id
-          post '/api/v1/meeting_rooms', params: valid_attribues
+          post '/api/v1/meeting_rooms', params: invalid_attribues
           json = JSON.parse(response.body).deep_symbolize_keys
           expect(json[:message]).to eq("Couldn't find OfficeManager")
           expect(json[:code]).to eq(404)
@@ -27,8 +31,9 @@ RSpec.describe 'Api::V1::MeetingRooms', type: :request do
 
       context 'When User is not admin of office' do
         it 'raise You are not admin of offcies' do
-          valid_attribues[:office_id] = office_1.id
-          post '/api/v1/meeting_rooms', params: valid_attribues
+          invalid_attribues[:office_id] = office_1.id
+          invalid_attribues[:user_id] = admin_2.id
+          post '/api/v1/meeting_rooms', params: invalid_attribues
           json = JSON.parse(response.body).deep_symbolize_keys
           expect(json[:message]).to eq('You are not admin of offcies')
           expect(json[:code]).to eq(401)
@@ -36,12 +41,14 @@ RSpec.describe 'Api::V1::MeetingRooms', type: :request do
       end
     end
 
-    context 'Validation' do
+    context 'When invalid data' do
       context 'When param is blank' do
         it 'raise Validation failed: Name can not be blank, Seat can not be blank' do
-          valid_attribues[:name] = nil
-          valid_attribues[:seat] = nil
-          post '/api/v1/meeting_rooms', params: valid_attribues
+          invalid_attribues[:name] = nil
+          invalid_attribues[:seat] = nil
+          invalid_attribues[:office_id] = office_1.id
+          invalid_attribues[:user_id] = admin_1.id
+          post '/api/v1/meeting_rooms', params: invalid_attribues
           json = JSON.parse(response.body).deep_symbolize_keys
           expect(json[:message]).to eq("Validation failed: Name can't be blank, Seat can't be blank")
           expect(json[:code]).to eq(400)
@@ -50,24 +57,43 @@ RSpec.describe 'Api::V1::MeetingRooms', type: :request do
 
       context 'When param already exists' do
         it 'raise Validation failed: Name has already been taken' do
-          valid_attribues[:name] = meeting_room.name
-          post '/api/v1/meeting_rooms', params: valid_attribues
+          invalid_attribues[:name] = meeting_room.name
+          invalid_attribues[:user_id] = admin_1.id
+          invalid_attribues[:office_id] = office_1.id
+          post '/api/v1/meeting_rooms', params: invalid_attribues
           json = JSON.parse(response.body).deep_symbolize_keys
           expect(json[:message]).to eq('Validation failed: Name has already been taken')
           expect(json[:code]).to eq(400)
         end
       end
+
+      context 'When param already exists office 1 but create office 2' do
+        it 'Created meeting room office 2' do
+          invalid_attribues[:name] = meeting_room.name
+          invalid_attribues[:user_id] = admin_2.id
+          invalid_attribues[:office_id] = office_2.id
+          post '/api/v1/meeting_rooms', params: invalid_attribues
+          json = JSON.parse(response.body).deep_symbolize_keys
+          expect(json[:data][:name]).to eq(meeting_room.name)
+          expect(json[:data][:office_name]).to eq(office_2.name)
+          expect(json[:data][:username]).to eq(admin_2.name)
+          expect(json[:meta][:message]).to eq('Successfully')
+          expect(json[:meta][:status]).to eq(200)
+          expect(MeetingRoom.last.name).to eq(meeting_room.name)
+        end
+      end
     end
 
-    context 'Created' do
+    context 'When valid data' do
       it 'create meeting room' do
         post '/api/v1/meeting_rooms', params: valid_attribues
         json = JSON.parse(response.body).deep_symbolize_keys
         expect(json[:data][:name]).to eq(valid_attribues[:name])
-        expect(json[:data][:office_name]).to eq(office_2.name)
-        expect(json[:data][:username]).to eq(admin.name)
+        expect(json[:data][:office_name]).to eq(office_1.name)
+        expect(json[:data][:username]).to eq(admin_1.name)
         expect(json[:meta][:message]).to eq('Successfully')
         expect(json[:meta][:status]).to eq(200)
+        expect(MeetingRoom.last.name).to eq(valid_attribues[:name])
       end
     end
   end
